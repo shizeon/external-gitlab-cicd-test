b5953861cc2c6981a8a46f9c3668bd071ebf1488
Sean Bridges <sbridges@ruvos.com>
2024-06-25 07:05:46 -0700

    feat: Do first push with java tests

---
+++ src/main/java/com/drajer/ecr/anonymizer/AnonymizerLambdaFunctionHandler.java
@@ -0,0 +1,393 @@
+package com.drajer.ecr.anonymizer;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.Collections;
+import java.util.Date;
+import java.util.Map;
+import java.util.UUID;
+
+import org.hl7.fhir.r4.model.Bundle;
+import org.hl7.fhir.r4.model.Coding;
+import org.hl7.fhir.r4.model.DateTimeType;
+import org.hl7.fhir.r4.model.DateType;
+import org.hl7.fhir.r4.model.Identifier;
+import org.hl7.fhir.r4.model.Observation;
+import org.hl7.fhir.r4.model.Patient;
+import org.hl7.fhir.r4.model.Quantity;
+import org.hl7.fhir.r4.model.Reference;
+import org.hl7.fhir.r4.model.Resource;
+import org.hl7.fhir.r4.model.Bundle.BundleEntryComponent;
+import org.hl7.fhir.r4.model.Enumerations.ResourceType;
+import org.springframework.util.ResourceUtils;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.SQSEvent;
+import com.amazonaws.services.lambda.runtime.events.SQSEvent.SQSMessage;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.event.S3EventNotification;
+import com.amazonaws.services.s3.model.GetObjectRequest;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.util.StringUtils;
+import com.drajer.ecr.anonymizer.service.AnonymizerService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.parser.DataFormatException;
+import ca.uhn.fhir.parser.IParser;
+import net.sf.saxon.Transform;
+
+public class AnonymizerLambdaFunctionHandler implements RequestHandler<SQSEvent, String> {
+	private AmazonS3 s3Client = AmazonS3ClientBuilder.standard().build();
+	private final String LOINC_URL = "http://loinc.org";
+	private String destPath = System.getProperty("java.io.tmpdir");
+	public static final int DEFAULT_BUFFER_SIZE = 8192;
+
+	@Override
+	public String handleRequest(SQSEvent event, Context context) {
+		// call ccda -- to fhir object
+		// convert fhir object to anonymizer
+		// write anonymizer to bucket
+		try {
+			SQSMessage message = event.getRecords().get(0);
+	        String messageBody = message.getBody();
+	        S3EventNotification s3EventNotification = S3EventNotification.parseJson(messageBody);
+	        S3EventNotification.S3EventNotificationRecord record = s3EventNotification.getRecords().get(0);
+			
+	        String bucket = record.getS3().getBucket().getName();
+	        String key = record.getS3().getObject().getKey();			
+			
+			
+			context.getLogger().log("EventName:" + record.getEventName());
+			context.getLogger().log("BucketName:" + bucket);
+			context.getLogger().log("RR Key:" + key);
+			String metaDataFileName = "";
+			if (key.contains("RR")) {
+				metaDataFileName = key.replace("RR", "METADATA");
+			}else {
+				metaDataFileName = key.replace("rr", "metadata");
+			}
+			context.getLogger().log("metaDataFileName : " + metaDataFileName);
+			//get metadata json
+			InputStream metadataInputStream = getObject(bucket,metaDataFileName);
+			// get metdata map
+			Map<String, Object> metaDataMap = streamToMap(metadataInputStream);
+			// process RR
+			Bundle rrBundle = processEvent(bucket, key,context,metaDataMap,false);
+			if (key.contains("RR")) {
+				key = key.replace("RR", "EICR");
+			}else {
+				key = key.replace("rr", "eicr");
+			}
+			context.getLogger().log("EICR Key:" + key);
+			context.getLogger().log("metaDataMap : " + metaDataMap.toString());
+			//process EICR
+			Bundle eicrBundle = processEvent(bucket, key,context,metaDataMap,true);
+			AnonymizerService anonymizerService = new AnonymizerService();
+			Bundle eicrRRBundle = anonymizerService.addReportabilityResponseInformationSection(eicrBundle, rrBundle, metaDataMap);
+
+			String uniqueFilename = "OUTPUT-"+ key;
+			IParser parser = FhirContext.forR4().newXmlParser();
+
+			String processedDataBundleXml = parser.setPrettyPrint(true).encodeResourceToString(eicrRRBundle);
+
+			context.getLogger().log("Anonymizer file name : "+uniqueFilename);
+			
+			if (StringUtils.isNullOrEmpty(processedDataBundleXml)) {
+				context.getLogger().log("Output not generated check logs ");
+			} else {
+				context.getLogger().log("Writing output file "+uniqueFilename);
+				this.writeFile(processedDataBundleXml, bucket, uniqueFilename, context);
+				context.getLogger().log("Output Generated  " + bucket + "/" + uniqueFilename);
+			}			
+			return "SUCCESS";
+		} catch (Exception e) {
+			context.getLogger().log(e.getMessage());
+			e.printStackTrace();
+			return "ERROR:" + e.getMessage();
+		} finally {
+		}
+	}
+	
+	private Bundle processEvent(String bucket, String key, Context context,Map<String, Object> metaDataMap,boolean addAgeObservationEntry) throws Exception{
+		InputStream input = null;
+		File outputFile = null;
+		String keyFileName = "";
+		try {
+			if (key != null && key.indexOf(File.separator) != -1) {
+				keyFileName = key.substring(key.lastIndexOf(File.separator));
+			} else {
+				keyFileName = key;
+			}
+
+			context.getLogger().log("JVM - Temp Folder Path:::" + destPath);
+
+			if (!this.isConverterBucket(bucket)) {
+				context.getLogger().log(
+						"BUCKET_NAME env null; Env BUCKET_NAME should match the bucket name created for converter ");
+				throw new RuntimeException ("Error: Different Bucket ");
+			}
+
+			S3Object s3Object = s3Client.getObject(new GetObjectRequest(bucket, key));
+			input = s3Object.getObjectContent();
+			outputFile = new File("/tmp/" + keyFileName);
+
+			outputFile.setWritable(true);
+
+			context.getLogger().log("Output File----" + outputFile.getAbsolutePath());
+			context.getLogger().log("Output File -- CanWrite?:" + outputFile.canWrite());
+			context.getLogger().log("Output File -- Length:" + outputFile.length());
+
+			try (FileOutputStream outputStream = new FileOutputStream(outputFile, false)) {
+				int read;
+				byte[] bytes = new byte[DEFAULT_BUFFER_SIZE];
+				while ((read = input.read(bytes)) != -1) {
+					outputStream.write(bytes, 0, read);
+				}
+				outputStream.close();
+			}
+
+			context.getLogger().log("Output File -- Length:" + outputFile.length());
+			context.getLogger().log("---- s3Object-Content....:" + s3Object.getObjectMetadata().getContentType());
+
+			UUID randomUUID = UUID.randomUUID();
+			File xsltFile = ResourceUtils.getFile("classpath:hl7-xml-transforms/transforms/cda2fhir-r4/NativeUUIDGen-cda2fhir.xslt");
+
+			context.getLogger().log("--- Before Transformation XSLT---::" + xsltFile.getAbsolutePath());
+			context.getLogger().log("--- Before Transformation OUTPUT---::" + outputFile.getAbsolutePath());
+			context.getLogger().log("--- Before Transformation UUID---::" + randomUUID);
+
+			xsltTransformation(xsltFile.getAbsolutePath(), outputFile.getAbsolutePath(), randomUUID, context);
+
+			String responseXML = getFileContentAsString(randomUUID, context);
+			
+			String fileName = key;
+			context.getLogger().log("Before file name : "+fileName);
+			context.getLogger().log("key toLowerCase contains rr: "  +key.toLowerCase().contains("rr"));
+			
+			if (key.toLowerCase().contains("rr")) {
+				fileName = key.replace("RR","FHIR_RR");
+			}else {
+				fileName = key.replace("EICR","FHIR_EICR");
+			}
+
+			context.getLogger().log("FHIR file name : "+fileName);
+			//writing fhir xml
+			if (StringUtils.isNullOrEmpty(responseXML)) {
+				context.getLogger().log("Output not generated check logs ");
+			} else {
+				context.getLogger().log("Writing FHIR output file "+fileName);
+				this.writeFile(responseXML, bucket, fileName, context);
+				context.getLogger().log("FHIR Output Generated  "+bucket+"/"+fileName);
+			}
+			
+			if (addAgeObservationEntry) {
+				responseXML = addAgeObservationBundleEntry(responseXML);
+			}
+			
+			AnonymizerService anonymizerService = new AnonymizerService();
+			return anonymizerService.processBundleXml(responseXML,metaDataMap);
+
+		} catch (Exception e) {
+			context.getLogger().log(e.getMessage());
+			e.printStackTrace();
+			throw new RuntimeException ("Error:  ",e);
+		} finally {
+			try {
+				input.close();
+			} catch (Exception e) {
+			}
+		}
+	}
+
+	public String addAgeObservationBundleEntry(String bundleXml) {
+		IParser xmlParser = FhirContext.forR4().newXmlParser();
+
+		try {
+			Bundle bundle = xmlParser.parseResource(Bundle.class, bundleXml);
+
+			BundleEntryComponent patientEntry = getBundleEntryByType(bundle, ResourceType.PATIENT.toCode());
+			Patient patient = patientEntry != null ? (Patient) patientEntry.getResource() : null;
+
+			if (patient != null && patient.hasBirthDateElement()) {
+				BundleEntryComponent observationEntry = new BundleEntryComponent();
+				observationEntry.setFullUrl("urn:uuid:" + getGuid());
+				Observation ageObservation = createAgeObservationResource(patient, patientEntry.getFullUrl());
+				observationEntry.setResource(ageObservation);
+				bundle.addEntry(observationEntry);
+			}
+
+			return xmlParser.setPrettyPrint(true).encodeResourceToString(bundle);
+
+		} catch (DataFormatException e) {
+			String errorMessage = "Failed to parse XML: " + e.getMessage();
+			throw new RuntimeException( errorMessage, e);
+		} catch (Exception e) {
+			String errorMessage = "An unexpected error occurred while processing the XML bundle: " + e.getMessage();
+			throw new RuntimeException( errorMessage, e);
+		}
+	}
+	
+	public BundleEntryComponent getBundleEntryByType(Bundle bundle, String resourceType) {
+
+		// Iterate through the entries in the bundle
+		for (BundleEntryComponent entry : bundle.getEntry()) {
+			Resource resource = entry.getResource();
+			if (resource.getResourceType().name().equals(resourceType)) {
+
+				return entry;
+			}
+		}
+
+		return null;
+	}
+	
+	public Observation createAgeObservationResource(Patient patient, String patientUrl) {
+		Observation observation = new Observation();
+		observation.setIdentifier(Collections.singletonList(generateIdentifier()));
+		observation.setStatus(Observation.ObservationStatus.FINAL);
+
+		Coding coding = new Coding();
+		coding.setSystem(LOINC_URL);
+		coding.setCode("29553-5");
+		coding.setDisplay("Age calculated");
+		observation.getCode().addCoding(coding);
+
+		observation.setEffective(new DateTimeType(new Date()));
+
+		Reference subjectReference = new Reference(patientUrl);
+		observation.setSubject(subjectReference);
+
+		Quantity ageQuantity = new Quantity();
+		ageQuantity.setCode("a");
+		ageQuantity.setValue(calculateAge(patient.getBirthDateElement()));
+		ageQuantity.setUnit("yr");
+		ageQuantity.setSystem("http://unitsofmeasure.org");
+
+		observation.setValue(ageQuantity);
+		return observation;
+	}
+
+	private Identifier generateIdentifier() {
+		Identifier identifier = new Identifier();
+		identifier.setSystem("urn:ietf:rfc:3986");
+		identifier.setValue("urn:uuid:" + getGuid());
+		return identifier;
+	}
+
+	public static String getGuid() {
+		return java.util.UUID.randomUUID().toString();
+	}
+	
+	private static int calculateAge(DateType birthDate) {
+		LocalDate birthLocalDate = birthDate.getValueAsCalendar().toInstant().atZone(java.time.ZoneId.systemDefault())
+				.toLocalDate();
+		return Period.between(birthLocalDate, LocalDate.now()).getYears();
+	}
+	
+	/**
+	 * Check if the S3 file processing is from the same S3 Converter bucket
+	 * 
+	 * @param theBucketName
+	 * @return
+	 */
+	private boolean isConverterBucket(String theBucketName) {
+
+		String envBucketName = System.getenv("BUCKET_NAME");
+		if (envBucketName != null && theBucketName != null && theBucketName.equalsIgnoreCase(envBucketName)) {
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * Below method is used to call the Saxon transform method (i.e main method)
+	 * 
+	 * @param xslFilePath
+	 * @param sourceXml
+	 * @param outputFileName
+	 */
+	private void xsltTransformation(String xslFilePath, String sourceXml, UUID outputFileName, Context context) {
+
+		try {
+
+			String[] commandLineArguments = new String[3];
+
+			commandLineArguments[0] = "-xsl:" + xslFilePath;
+			commandLineArguments[1] = "-s:" + sourceXml;
+			// commandLineArguments[2] = "-license:on";
+			commandLineArguments[2] = "-o:" + "/tmp/" + outputFileName + ".xml";
+
+			Transform.main(commandLineArguments);
+
+			context.getLogger().log("Transformation Complete");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			context.getLogger().log("ERROR: Transformation Failed with exception " + e.getMessage());
+		}
+	}
+
+	private String getFileContentAsString(UUID fileName, Context context) {
+		File outputFile = null;
+		try {
+			outputFile = ResourceUtils.getFile("/tmp/" + fileName + ".xml");
+			String absolutePath = outputFile.getAbsolutePath();
+			byte[] readAllBytes = Files.readAllBytes(Paths.get(absolutePath));
+			Charset encoding = Charset.defaultCharset();
+			String string = new String(readAllBytes, encoding);
+			return string;
+		} catch (FileNotFoundException e) {
+			context.getLogger().log("ERROR: output file not found " + e.getMessage());
+		} catch (IOException e) {
+			context.getLogger().log("ERROR: IO Exception while reading output file " + e.getMessage());
+		} catch (Exception ee) {
+			context.getLogger().log("ERROR: Exception for output " + ee.getMessage());
+		}
+		return null;
+	}
+
+	private void writeFile(String fileContent, String bucketName, String keyPrefix, Context context) {
+		try {
+			byte[] contentAsBytes = fileContent.getBytes("UTF-8");
+			ByteArrayInputStream is = new ByteArrayInputStream(contentAsBytes);
+			ObjectMetadata meta = new ObjectMetadata();
+			meta.setContentLength(contentAsBytes.length);
+			meta.setContentType("text/xml");
+
+			context.getLogger().log("bucketName ::::"+bucketName);
+			context.getLogger().log("meta ::::"+keyPrefix + meta.toString());
+			
+			// Uploading to S3 destination bucket
+			s3Client.putObject(bucketName, keyPrefix , is, meta);
+			is.close();
+		} catch (Exception e) {
+			context.getLogger().log("ERROR:" + e.getMessage());
+			e.printStackTrace();
+		}
+	}
+	
+	private InputStream getObject(String bucket, String key) throws IOException {
+		S3Object s3Object = s3Client.getObject(new GetObjectRequest(bucket, key));
+		InputStream inputStream = s3Object.getObjectContent();
+		return inputStream;
+	}	
+	
+	private Map<String, Object> streamToMap(InputStream inputStream) throws IOException {
+		ObjectMapper objectMapper = new ObjectMapper();
+		return objectMapper.readValue(inputStream, Map.class);
+	}
+	
+}
---
+++ src/main/resources/hl7-xml-transforms/transforms/cda2fhir-r4/cda2fhir-Condition.xslt
@@ -0,0 +1,247 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<xsl:stylesheet xmlns="http://hl7.org/fhir" xmlns:xsl="http://www.w3.org/1999/XSL/Transform" xmlns:cda="urn:hl7-org:v3" xmlns:fhir="http://hl7.org/fhir" xmlns:sdtc="urn:hl7-org:sdtc"
+    xmlns:xs="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:lcg="http://www.lantanagroup.com"
+    exclude-result-prefixes="lcg xsl cda fhir xs xsi sdtc xhtml" version="2.0">
+
+    <!-- Create bundle entries for the Problem Observation (Condition) and any authors or performers -->
+    <xsl:template match="cda:observation[cda:templateId[@root = '2.16.840.1.113883.10.20.22.4.4']]" mode="bundle-entry">
+        <xsl:call-template name="create-bundle-entry" />
+
+        <xsl:apply-templates select="cda:author" mode="bundle-entry" />
+        <xsl:apply-templates select="cda:informant" mode="bundle-entry" />
+        <xsl:apply-templates select="cda:performer" mode="bundle-entry" />
+
+        <xsl:for-each select="cda:author[position() > 1] | cda:informant | cda:performer[position() > 1]">
+            <xsl:apply-templates select="." mode="provenance" />
+        </xsl:for-each>
+
+        <xsl:apply-templates select="cda:entryRelationship/cda:*" mode="bundle-entry" />
+    </xsl:template>
+
+    <!-- TEMPLATE: Remove Concern wrappers -->
+    <xsl:template
+        match="cda:act[cda:templateId[@root = '2.16.840.1.113883.10.20.22.4.132'] or cda:templateId[@root = '2.16.840.1.113883.10.20.22.4.3'] or cda:templateId[@root = '2.16.840.1.113883.10.20.22.4.30'] or cda:templateId[@root = '2.16.840.1.113883.10.20.22.4.136']]"
+        mode="reference">
+        <xsl:param name="wrapping-elements" />
+        <xsl:for-each select="cda:entryRelationship/cda:*[not(@nullFlavor)]">
+            <xsl:apply-templates select="." mode="reference">
+                <xsl:with-param name="wrapping-elements" select="$wrapping-elements" />
+            </xsl:apply-templates>
+        </xsl:for-each>
+    </xsl:template>
+
+    <!-- TEMPLATE: Remove Concern wrappers -->
+    <xsl:template
+        match="cda:act[cda:templateId[@root = '2.16.840.1.113883.10.20.22.4.132'] or cda:templateId[@root = '2.16.840.1.113883.10.20.22.4.3'] or cda:templateId[@root = '2.16.840.1.113883.10.20.22.4.30'] or cda:templateId[@root = '2.16.840.1.113883.10.20.22.4.136']]"
+        mode="bundle-entry">
+        <!-- Create bundle entries for any authors or performers -->
+        <xsl:apply-templates select="cda:author" mode="bundle-entry" />
+        <xsl:apply-templates select="cda:performer" mode="bundle-entry" />
+        <!-- Create provenance entries for any authors or performers -->
+        <xsl:apply-templates select="cda:author" mode="provenance" />
+        <xsl:apply-templates select="cda:performer" mode="provenance" />
+        <xsl:for-each select="cda:entryRelationship/cda:*[not(@nullFlavor)]">
+            <xsl:apply-templates select="." mode="bundle-entry" />
+        </xsl:for-each>
+    </xsl:template>
+
+    <!-- C-CDA Problem Observation -->
+    <xsl:template match="cda:observation[cda:templateId[@root = '2.16.840.1.113883.10.20.22.4.4']]">
+        <Condition xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://hl7.org/fhir">
+            <!-- Check current Ig -->
+            <xsl:variable name="vCurrentIg">
+                <xsl:apply-templates select="/" mode="currentIg" />
+            </xsl:variable>
+
+            <!-- Set profiles based on Ig and Resource if it is needed -->
+            <xsl:choose>
+                <xsl:when test="$vCurrentIg = 'NA'">
+                    <xsl:call-template name="add-meta" />
+                </xsl:when>
+                <xsl:otherwise>
+                    <xsl:variable name="vProfileValue">
+                        <xsl:call-template name="get-profile-for-ig">
+                            <xsl:with-param name="pIg" select="$vCurrentIg" />
+                            <xsl:with-param name="pResource" select="'Condition'" />
+                        </xsl:call-template>
+                    </xsl:variable>
+                    <xsl:choose>
+                        <xsl:when test="$vProfileValue ne 'NA'">
+                            <meta>
+                                <profile>
+                                    <xsl:attribute name="value">
+                                        <xsl:value-of select="$vProfileValue" />
+                                    </xsl:attribute>
+                                </profile>
+                            </meta>
+                        </xsl:when>
+                    </xsl:choose>
+                </xsl:otherwise>
+            </xsl:choose>
+
+            <xsl:apply-templates select="cda:id" />
+            <!-- clinicalStatus -->
+            <clinicalStatus>
+                <coding>
+                    <system value="http://terminology.hl7.org/CodeSystem/condition-clinical" />
+                    <code>
+                        <xsl:choose>
+                            <xsl:when test="cda:effectiveTime/cda:high">
+                                <xsl:attribute name="value">resolved</xsl:attribute>
+                            </xsl:when>
+                            <xsl:otherwise>
+                                <xsl:attribute name="value">active</xsl:attribute>
+                            </xsl:otherwise>
+                        </xsl:choose>
+                    </code>
+                </coding>
+            </clinicalStatus>
+            <!-- SG 2024-02-05: Updated negationInd processing for eCR -->
+            <xsl:choose>
+                <xsl:when test="@negationInd = 'true' and cda:templateId[@root = '2.16.840.1.113883.10.20.15.2.3.3']">
+                    <verificationStatus>
+                        <coding>
+                            <system value="http://terminology.hl7.org/CodeSystem/condition-ver-status" />
+                            <code value="entered-in-error" />
+                        </coding>
+                    </verificationStatus>
+                </xsl:when>
+                <xsl:when test="@negationInd = 'true' and not(cda:value/@code = '55607006')">
+                    <verificationStatus>
+                        <coding>
+                            <system value="http://terminology.hl7.org/CodeSystem/condition-ver-status" />
+                            <code value="refuted" />
+                        </coding>
+                    </verificationStatus>
+                </xsl:when>
+            </xsl:choose>
+
+            <xsl:choose>
+                <xsl:when test="ancestor::cda:act[cda:templateId[@root = '2.16.840.1.113883.10.20.22.4.80']]">
+                    <category>
+                        <coding>
+                            <system value="http://terminology.hl7.org/CodeSystem/condition-category" />
+                            <code value="encounter-diagnosis" />
+                        </coding>
+                    </category>
+                </xsl:when>
+                <xsl:otherwise>
+                    <category>
+                        <coding>
+                            <system value="http://terminology.hl7.org/CodeSystem/condition-category" />
+                            <code value="problem-list-item" />
+                        </coding>
+                    </category>
+                </xsl:otherwise>
+                
+            </xsl:choose>
+
+            <xsl:apply-templates select="cda:value" mode="condition" />
+
+            <xsl:call-template name="subject-reference" />
+            <xsl:apply-templates select="cda:effectiveTime" mode="condition" />
+
+            <!-- recordedDate (max 1) -->
+            <xsl:choose>
+                <xsl:when test="cda:author[1]/cda:time">
+                    <xsl:apply-templates select="cda:author[1]/cda:time" mode="instant">
+                        <xsl:with-param name="pElementName">recordedDate</xsl:with-param>
+                    </xsl:apply-templates>        
+                </xsl:when>
+                <xsl:when test="ancestor::cda:act[cda:templateId[@root = '2.16.840.1.113883.10.20.22.4.3']]/cda:effectiveTime/cda:low/@value">
+                    <xsl:apply-templates select="ancestor::cda:act[cda:templateId[@root = '2.16.840.1.113883.10.20.22.4.3']]/cda:effectiveTime" mode="instant">
+                        <xsl:with-param name="pElementName">recordedDate</xsl:with-param>
+                    </xsl:apply-templates>        
+                </xsl:when>
+            </xsl:choose>
+            
+            <!-- recorder (max 1)-->
+            <xsl:apply-templates select="cda:author[1]" mode="rename-reference-participant">
+                <xsl:with-param name="pElementName">recorder</xsl:with-param>
+            </xsl:apply-templates>
+
+            <!-- asserter (max 1)-->
+            <xsl:apply-templates select="cda:performer[1]" mode="rename-reference-participant">
+                <xsl:with-param name="pElementName">asserter</xsl:with-param>
+            </xsl:apply-templates>
+
+            <xsl:for-each select="cda:entryRelationship[@typeCode = 'REFR']/cda:act[cda:templateId/@root = '2.16.840.1.113883.10.20.22.4.122']">
+                <xsl:apply-templates select="." mode="reference">
+                    <xsl:with-param name="wrapping-elements">evidence/detail</xsl:with-param>
+                </xsl:apply-templates>
+            </xsl:for-each>
+            <xsl:if test="cda:text">
+                <xsl:variable name="text">
+                    <xsl:apply-templates select="cda:text" />
+                </xsl:variable>
+                <xsl:if test="string-length($text) &gt; 0">
+                    <note>
+                        <text value="{normalize-space(cda:text)}" />
+                    </note>
+                </xsl:if>
+            </xsl:if>
+            <xsl:if test="@negationInd = 'true' and not(cda:value/@code = '55607006')">
+                <note>
+                    <text value="This condition was converted from a C-CDA document. It was marked as negated in that file, so marked as refuted in FHIR" />
+                </note>
+            </xsl:if>
+        </Condition>
+    </xsl:template>
+
+    <!-- C-CDA Problem Status -->
+    <xsl:template match="cda:observation[cda:templateId/@root = '2.16.840.1.113883.10.20.22.4.6']" mode="condition">
+        <xsl:for-each select="cda:value">
+            <clinicalStatus>
+                <coding>
+                    <system value="http://terminology.hl7.org/CodeSystem/condition-clinical" />
+
+                    <xsl:choose>
+                        <xsl:when test="@code = '55561003'">
+                            <code value="active" />
+                        </xsl:when>
+                        <xsl:when test="@code = '73425007'">
+                            <code value="inactive" />
+                        </xsl:when>
+                        <xsl:when test="@code = '413322009'">
+                            <code value="resolved" />
+                        </xsl:when>
+                    </xsl:choose>
+                </coding>
+            </clinicalStatus>
+        </xsl:for-each>
+    </xsl:template>
+
+    <xsl:template match="cda:effectiveTime" mode="condition">
+        <xsl:if test="cda:low/@value">
+            <onsetDateTime value="{lcg:cdaTS2date(cda:low/@value)}" />
+        </xsl:if>
+        <xsl:if test="cda:high/@value">
+            <abatementDateTime value="{lcg:cdaTS2date(cda:high/@value)}" />
+        </xsl:if>
+    </xsl:template>
+
+    <xsl:template match="cda:code" mode="condition">
+        <xsl:call-template name="newCreateCodableConcept">
+            <xsl:with-param name="pElementName">category</xsl:with-param>
+        </xsl:call-template>
+    </xsl:template>
+
+    <xsl:template match="cda:value" mode="condition">
+        <xsl:choose>
+            <xsl:when test="../@negationInd = 'true' and @code = '55607006'">
+                <code>
+                    <coding>
+                        <system value="http://snomed.info/sct" />
+                        <code value="160245001" />
+                        <display value="No known problems" />
+                    </coding>
+                </code>
+            </xsl:when>
+            <xsl:otherwise>
+                <xsl:call-template name="newCreateCodableConcept">
+                    <xsl:with-param name="pElementName">code</xsl:with-param>
+                </xsl:call-template>
+            </xsl:otherwise>
+        </xsl:choose>
+    </xsl:template>
+</xsl:stylesheet>
